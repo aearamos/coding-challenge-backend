4f3491a504015b3551a60a083eab662e830d8d4d
Arthur Ramos <ramos.oceano@gmail.com>
2017-11-28 21:20:27 +0100

    created_zombies_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'zombies/index'
+
+  get 'zombies/new'
+
+  get 'zombies/create'
+
+  get 'zombies/show'
+
+  get 'zombies/edit'
+
+  get 'zombies/update'
+
+  get 'zombies/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
