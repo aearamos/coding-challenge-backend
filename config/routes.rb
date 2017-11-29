9503104276ad2940598af319e6d8fb8e97787ced
Arthur Ramos <ramos.oceano@gmail.com>
2017-11-29 21:57:18 +0100

    zombies_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
 
-  root to: 'pages#index'
 
   get 'pages/index'
-
+  root to: 'pages#index'
   resources :zombies
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
