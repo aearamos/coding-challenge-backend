ecf412121fe979863e6d4f8c1d6284f8a7647fd9
Arthur Ramos <ramos.oceano@gmail.com>
2017-12-13 02:49:39 +0100

    weapon_armor_working

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
 
-  resources :zombies
-
-  root to: 'pages#home'
-
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :zombies
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
