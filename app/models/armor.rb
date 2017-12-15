7f46b211b963517e5cfecb84c03c4b32d8c9b529
Arthur Ramos <ramos.oceano@gmail.com>
2017-12-15 20:21:17 +0100

    readme_file

--- app/models/armor.rb
+++ app/models/armor.rb
@@ -1,3 +1,8 @@
 class Armor < ApplicationRecord
   has_many :zombie_armors
+
+  validates :name, presence: :true
+  validates :defense_points, presence: :true
+  validates :durability, presence: :true
+  validates :price, presence: :true
 end
